45ca503c104d238105f3ffd4b409aac92ab6f8ce
Ray Bellis <ray@isc.org>
2018-04-13 15:33:23 +0100

    make subnet lengths configurable
    removed null plugin methods

--- plugins/anonzero/anonzero.c
+++ plugins/anonzero/anonzero.c
@@ -44,12 +44,45 @@
 #include "dnscap_common.h"
 
 static uint16_t port = 53;
+static int mask4_bits = 24;
+static int mask6_bits = 56;
+
+static struct in_addr mask4;
+static struct in6_addr mask6;
 
 void anonzero_usage()
 {
     fprintf(stderr,
         "\nanonzero.so options:\n"
-        "\t-u <port>    dns port (default: 53)\n");
+        "\t-u <port>    dns port (default: 53)\n"
+        "\t-4 <bits>    mask length for IPv4 (default: 24)\n"
+        "\t-6 <bits>    mask length for IPv6 (default: 56)\n");
+}
+
+static void anonzero_make_mask(uint8_t* p, int bits)
+{
+    for (int i = 0; i < bits; ++i) {
+        int offset = i / 8;
+        int bit = 7 - (i % 8);
+        p[offset] |= (1 << bit);
+    }
+}
+
+static void anonzero_mask_ipaddr(iaddr* ip)
+{
+    uint8_t* p = (uint8_t*)(&ip->u);
+
+    if (AF_INET == ip->af) {
+        uint8_t* q = (uint8_t*)&mask4;
+        for (int i = 0; i < sizeof(mask4); ++i) {
+            p[i] &= q[i];
+        }
+    } else if (AF_INET6 == ip->af) {
+        uint8_t* q = (uint8_t*)&mask6;
+        for (int i = 0; i < sizeof(mask6); ++i) {
+            p[i] &= q[i];
+        }
+    }
 }
 
 void anonzero_getopt(int* argc, char** argv[])
@@ -57,7 +90,7 @@ void anonzero_getopt(int* argc, char** argv[])
     int c;
     unsigned long ul;
     char *p;
-    while ((c = getopt(*argc, *argv, "u:")) != EOF) {
+    while ((c = getopt(*argc, *argv, "u:4:6:")) != EOF) {
         switch (c) {
         case 'u':
             ul = strtoul(optarg, &p, 0);
@@ -67,43 +100,33 @@ void anonzero_getopt(int* argc, char** argv[])
             }
             port = (uint16_t)ul;
             break;
+        case '4':
+            ul = strtoul(optarg, &p, 0);
+            if (*p != '\0' || ul < 0U || ul > 32U) {
+                fprintf(stderr, "IPv4 mask must be an integer 0..32");
+                exit(1);
+            }
+            mask4_bits = (int)ul;
+            break;
+        case '6':
+            ul = strtoul(optarg, &p, 0);
+            if (*p != '\0' || ul < 0U || ul > 32U) {
+                fprintf(stderr, "IPv6 mask must be an integer 0..128");
+                exit(1);
+            }
+            mask6_bits = (int)ul;
+            break;
         default:
             anonzero_usage();
             exit(1);
         }
     }
-}
 
-int anonzero_start(logerr_t* a_logerr)
-{
-    return 0;
-}
+    memset(&mask4, 0, sizeof(struct in_addr));
+    anonzero_make_mask((uint8_t*)&mask4, mask4_bits);
 
-void anonzero_stop()
-{
-}
-
-int anonzero_open(my_bpftimeval ts)
-{
-    return 0;
-}
-
-int anonzero_close(my_bpftimeval ts)
-{
-    return 0;
-}
-
-static void truncate_iaddr(iaddr* ip)
-{
-    uint8_t* p = (uint8_t*)(&ip->u);
-
-    if (AF_INET == ip->af) {
-        /* overwrite the last octet of the address */
-        p[3] = 0;
-    } else if (AF_INET6 == ip->af) {
-        /* overwrite the last 9 octets of the address (/56) */
-        memset(p + 7, 0, 9);
-    }
+    memset(&mask6, 0, sizeof(struct in6_addr));
+    anonzero_make_mask((uint8_t*)&mask6, mask6_bits);
 }
 
 void anonzero_output(const char* descr, iaddr* from, iaddr* to, uint8_t proto, unsigned flags,
@@ -113,9 +136,8 @@ void anonzero_output(const char* descr, iaddr* from, iaddr* to, uint8_t proto, u
 {
     /* assume traffic direction based on port */
     if (dport == port) {
-        truncate_iaddr(from);
-    }
-    if (sport == port) {
-        truncate_iaddr(to);
+        anonzero_mask_ipaddr(from);
+    } else if (sport == port) {
+        anonzero_mask_ipaddr(to);
     }
 }
