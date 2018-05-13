770168a9cb90965699de7ad2112f7565b973a838
Jerry Lundström <lundstrom.jerry@gmail.com>
2018-05-13 15:30:42 +0200

    Test
    - `src/test/test9.sh`: Add back layers test now that `-B` also affects that

--- src/test/test9.sh
+++ src/test/test9.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -xe
 
 ../dnscap -r dns.pcap-dist -g -B '2016-10-20 15:23:30' -E '2016-10-20 15:24:00' 2>test9.out
+../dnscap -r dns.pcap-dist -o use_layers=yes -g -B '2016-10-20 15:23:30' -E '2016-10-20 15:24:00' 2>>test9.out
 
 osrel=`uname -s`
 if [ "$osrel" = "OpenBSD" ]; then
