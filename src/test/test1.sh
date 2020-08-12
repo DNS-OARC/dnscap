19c71200e2949334f6012430643292c6724946eb
Jerry Lundström <lundstrom.jerry@gmail.com>
2020-08-12 16:47:17 +0200

    Coverage
    - Add `--enable-gcov` and `gcov` make target for Code coverage

--- src/test/test1.sh
+++ src/test/test1.sh
@@ -9,4 +9,8 @@ if [ "$osrel" = "OpenBSD" ]; then
     rm dns.out.old
 fi
 
+mv dns.out dns.out.old
+grep -v "^libgcov profiling error:" dns.out.old > dns.out
+rm dns.out.old
+
 diff dns.out "$srcdir/dns.gold"
