37a072c1a9b251095d748d58fac1d5b3c392ff73
Jerry Lundström <lundstrom.jerry@gmail.com>
2021-02-12 17:14:45 +0100

    Release 2.0.0

--- src/tcpstate.h
+++ src/tcpstate.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2020, OARC, Inc.
+ * Copyright (c) 2018-2021, OARC, Inc.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
